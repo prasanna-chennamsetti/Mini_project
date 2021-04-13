082f31416abc9f3176a9ad844128364b35e9f64b
prasanna-chennamsetti <60134702+prasanna-chennamsetti@users.noreply.github.com>
2021-04-13 13:08:56 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,112 +1,77 @@
 #include <calculator_operations.h>
 
 /* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+#include<stdio.h>
+#include<math.h>
+int main()
 {
-    printf("\n****Calculator****\n");
-    while(1)
+    int a,b;
+    printf("Enter first value : ");
+    scanf("%d\n",&a);
+    printf("Enter Second value : ");
+    scanf("%d",&b);
+    char ch;
+    printf("Enter your choice : ");
+    scanf("%c",&ch);
+    float res=0;
+    switch(ch)
     {
-        calculator_menu();
-    }
-}
-
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     // __fpurge(stdin);
-    scanf("%d", &calculator_operation);
-
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        // __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        // __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
+        case '+':
+        add();
+        break;
+        case '-':
+        sub();
+        break;
+        case '*':
+        mult();
+        break;
+        case '/':
+        div();
+        break;
+        case '^':
+        power();
+        break;
+        case '%':
+        mod();
+        break;
         default:
-            printf("\n\t---It should never come here---\n");
+        printf("Invalid Operations");
     }
 }
-
-int valid_operation(int operation)
+void add(int a,int b)
 {
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+    int c=a+b;
+    printf("%d",c);
+}
+void sub(int a,int b){
+    int c=0;
+    if(a>b){
+        c=a-b;
+    }
+    else{
+        c=b-a;
+    }
+    printf("%d",c);
+}
+void mult(int a,int b){
+    int c=a*b;
+    printf("%d",c);
+}
+void div(int a,int b){
+    int c=0;
+    if(b==0){
+        printf("Invalid");
+    }
+    else{
+        c=a/b;
+    }
+    printf("%d",c);
+}
+void power(int a,int b){
+    int c=pow(a,b);
+    printf("%d",c);
+}
+void mod(int a,int b){
+    int c=a%b;
+    printf("%d",c);
 }
