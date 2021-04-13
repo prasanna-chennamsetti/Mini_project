a30417442600351ba9279d439c0ee72a6f29fac4
prasanna-chennamsetti <60134702+prasanna-chennamsetti@users.noreply.github.com>
2021-04-13 12:18:32 +0530

    Add files via upload

---
+++ calculator_operations.h
@@ -0,0 +1,48 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+#include<math.h>
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 + operand2
+*/
+void add(int a,int b)
+{
+	//Addition of elements
+	
+}
+void sub(int a,int b)
+{
+    
+    //Subtraction of elements
+}
+void mult(int a,int b)
+{
+    
+    //Multiplication of elements
+}
+void div(int a,int b)
+{
+
+   //Division of elements
+   
+}
+void power(int a,int b)
+{
+    //Power value of an element
+}
+void mod(int a,int b)
+{
+    //Modulus value of two values
+}
